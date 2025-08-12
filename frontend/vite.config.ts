3241dc7e6dfa7fcf35542a867f72609d8d44d1a4
lucena564 <victor_alb98@hotmail.com>
2025-08-12 00:07:14 -0300

    docker compose funcionando

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,11 +19,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',  // Isso faz o vite ouvir em todas as interfaces, não só localhost
+    port: 5173,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
